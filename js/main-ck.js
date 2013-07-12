d1da88e33e594d71dcafa99674d8a589fde5d2d2
hogshouse <hogshouse@gmail.com>
2013-07-12 19:29:28 +0100

    first play

---
+++ js/main-ck.js
@@ -0,0 +1,3 @@
+// check our environment
+// protohog.js include
+$mobCheck=$(".mobCheck");isMob=!1;$mobCheck.css("position")=="relative"&&(isMob=!0);isMob;
---
+++ js/main.js
@@ -0,0 +1,18 @@
+// check our environment
+
+// protohog.js include
+$mobCheck = $('.mobCheck');
+isMob = false;
+if ( $mobCheck.css('position') == 'relative' ) {
+	isMob = true;
+}
+
+
+if ( isMob ) {
+	// do something for mobile
+} else {
+	// do something for desktop
+}
+
+
+
